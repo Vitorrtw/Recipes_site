a2d1614e1c8aa972883a35667dcc786f3fe091fb
João Vitor Tavares <vitorrtw@hotmail.com>
2024-02-02 03:00:07 -0300

    initial Commit

---
+++ recipes/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='recipes-home'), 
+    path('recipes/category/<int:category_id>/', views.category, name='recipes-category'), 
+    path('recipes/<int:id>/', views.recipes, name='recipes-recipe'), 
+]
